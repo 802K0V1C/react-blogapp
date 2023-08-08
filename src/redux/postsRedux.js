c817a086ef358b808a26000dc8aa72fa70402e3f
Artur Bożek <arturhubert.bozek@gmail.com>
2023-08-08 18:54:52 +0200

    19.6

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -2,14 +2,17 @@
 export const getPostsById = ({ posts }, postId ) => posts.find((post) => post.id === postId)
 export const getAllPosts = ({ posts }) => posts;
 
+
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
 const GENERATE_POSTS = createActionName('GENERATE_POSTS');
 const REMOVE_CARD = createActionName('REMOVE_CARD');
+const ADD_POST = createActionName('ADD_POST');
 
 // action creators
 export const generatePosts = payload => ({ type: GENERATE_POSTS, payload,});
 export const removeCard = payload => ({ type: REMOVE_CARD, payload,});
+export const addPost = payload => ({ type: ADD_POST, payload,});
 
 const postsReducer = (statePart = [], action) => {
     switch (action.type) {
@@ -17,6 +20,8 @@ const postsReducer = (statePart = [], action) => {
             return [...statePart, ...action.payload];
             case REMOVE_CARD:
                 return statePart.filter((post) => post.id !== action.payload);
+                case ADD_POST:
+                    return [...statePart, { ...action.payload }]; 
             default:
                 return statePart;
 };
