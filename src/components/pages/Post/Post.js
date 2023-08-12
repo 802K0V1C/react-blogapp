4458d993436639850e14dff43e444b811d4f6c7a
Artur Bożek <arturhubert.bozek@gmail.com>
2023-08-12 18:07:53 +0200

    installed datepicker

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -47,7 +47,6 @@ return (
             <Card.Subtitle className="mb-2 text-muted">{renderPostById.publishedDate}</Card.Subtitle>
             <Card.Text className="pt-2 mb-0"><strong>Author:</strong> {renderPostById.author}</Card.Text>
             <Card.Text><strong>Description:</strong> {renderPostById.content}</Card.Text>
-            {/* dangerouslySetInnerHTML={{ __html: renderPostById.content }} */}
         </Card.Body>
         </Card>
     </Col>
