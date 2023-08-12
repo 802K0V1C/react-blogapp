df47348f9f1195c98cf098ffa803b031ca8703cb
Artur Bożek <arturhubert.bozek@gmail.com>
2023-08-12 17:53:23 +0200

    fix1

--- src/components/Features/PostForm.js
+++ src/components/Features/PostForm.js
@@ -2,49 +2,50 @@ import { useState } from 'react';
 import { Form, Button, Col } from 'react-bootstrap';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useForm } from 'react-hook-form';
 
 
-const PostForm = ({ action, actionText, ...props }) => {
-    const [title, setTitle] = useState(props.title ||'');
-    const [author, setAuthor] = useState(props.author ||'');
-    const [content, setContent] = useState(props.content ||'');
-    const [name, setName] = useState(props.name ||'');
-    const [content2, setContent2] = useState(props.content2 ||'');
+const PostForm = ({ action, actionText, ...props }) => { 
+    const [title, setTitle] = useState(props.title || '');
+    const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
+    const [author, setAuthor] = useState(props.author || '');
+    const [content, setContent] = useState(props.content || '');
+
+
+const { register, handleSubmit: validate, formState: { errors } } = useForm();
 
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    action({title, author, content, name, content2});
+    action({ title, author, publishedDate, content});
 };
 
 return (
     <div className="d-flex justify-content-center mt-5" >
         <Col xs={6} md={6} lg={6}>
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={validate(handleSubmit)}>
                 <Form.Group className="mb-3" controlId="postInformations">
                     
                     <Form.Label><strong>Title</strong></Form.Label>
-                    <Form.Control type="title" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Enter title" style={{ width: '70%' }}/>
+                    <Form.Control  {...register("title", { required: true, minLength: 3})} type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Enter title" style={{ width: '70%' }} />
+            {errors.title && <small className="d-block form-text text-danger">This field is required with at least 3 characters</small>}
 
                     <Form.Label className="pt-3"><strong>Author</strong></Form.Label>
-                    <Form.Control type="name" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Enter author" style={{ width: '70%' }}/>
+                    <Form.Control  {...register("date", { required: true, minLength: 3})} type="date" value={publishedDate} onChange={(e) => setPublishedDate(e.target.value)} placeholder="Enter date" style={{ width: '70%' }} />
                     
                     <Form.Label className="pt-3"><strong>Published</strong></Form.Label>
-                    <Form.Control type="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter author" style={{ width: '70%' }}/>
+                    <Form.Control {...register("author", { required: true, minLength: 3})} type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Enter name" style={{ width: '70%' }} />
+            {errors.author && <small className="d-block form-text text-danger">This field is required with at least 3 characters</small>}
                 </Form.Group>
                 
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label><strong>Short description</strong></Form.Label>
-                    <Form.Control as="textarea" value={content} onChange={(e) => setContent(e.target.value)} rows={5} placeholder="Leave a comment here ..."/>
-                    </Form.Group>
-                    
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea2">
-                <Form.Label><strong>Main content</strong></Form.Label>
-                    <Form.Control as="textarea" value={content2} onChange={(e) => setContent2(e.target.value)} rows={5} placeholder="Leave a comment here ..."/>
-                    <ReactQuill theme="snow"  placeholder='Write something here ...' value={content} onChange={setContent} style={{ height: '150px' }}></ReactQuill>
+                <Form.Label><strong>Content</strong></Form.Label>
+                <ReactQuill {...register("content", { required: true, maxLength: 20})} theme="snow"  placeholder='Write something here ...' value={content} onChange={setContent} style={{ height: '150px' }}></ReactQuill>
+              {/* <Form.Control as="textarea" value={content} onChange={(e) => setContent(e.target.value)} rows={5} placeholder="Write something here ..." /> */}   
+                {errors.content && <small className="mt-5 d-block form-text text-danger">This field is required with at least 3 characters and max 20</small>}
                 </Form.Group>
                 
-                <Button variant="info" type="submit" className='mt-5'>Add Post</Button>
+                <Button variant="info" type="submit" className="mt-5">Add Post</Button>
             </Form>
         </Col>
     </div>
