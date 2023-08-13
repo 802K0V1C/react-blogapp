b9480bf4dea345fedf36a0edb0a5a2a2cabc40da
Artur Bożek <arturhubert.bozek@gmail.com>
2023-08-13 21:00:19 +0200

    categories fix1

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -3,14 +3,12 @@ import { ListGroup, Col, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getCategories } from '../../redux/categoriesRedux';
 import { useParams } from 'react-router-dom';
-import { postsByCategory } from '../../redux/categoriesRedux';
-import PostList from '../Features/GenerateCards';
+import CategoryGenerateCards from '../Features/CategoryGenerateCards';
 
 const CategoryPage = () => {
     const { category } = useParams();
     const [selectedCategory, setSelectedCategory] = useState(category);
     const categories = useSelector(getCategories);
-    const posts = useSelector((state) => postsByCategory(state, selectedCategory));
 
     const handleCategoryClick = (categoryItem) => {
         setSelectedCategory(categoryItem);
@@ -18,9 +16,12 @@ const CategoryPage = () => {
 
 return (
     <div>
+        <header className="d-flex justify-content-between">
         <h1>Categories</h1>
+        <h1>Selected Category: <span>{selectedCategory}</span></h1>
+    </header>
     <Row>
-        <Col xs={6} md={6} lg={6}>
+    <Col className="mt-3">
         <ListGroup>
             {categories.map((categoryItem) => (
             <ListGroup.Item key={categoryItem}>
@@ -32,12 +33,9 @@ return (
         </ListGroup>
         </Col>
         <Col xs={6} md={6} lg={6}>
-        <h2>Selected Category: {selectedCategory}</h2>
-        <ul>
-            {posts.map((post) => (
-            <PostList title={post.title} author={post.author} publishedDate={post.publishedDate} content={post.content} category={post.category}></PostList>
-            ))}
-        </ul>
+        <div> 
+            <CategoryGenerateCards category={selectedCategory} />
+        </div>
         </Col>
     </Row>
     </div>
--- src/components/Features/CategoryGenerateCards.js
+++ src/components/Features/CategoryGenerateCards.js
@@ -1,28 +1,28 @@
-import {Card, Row, Button, Col} from 'react-bootstrap';
+import { Card, Row, Button, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-import { getAllPosts } from '../../redux/postsRedux';
+import { postsByCategory } from '../../redux/categoriesRedux';
 
-const GenerateCards = () => {
-    const posts = useSelector(getAllPosts);
+const CategoryGenerateCards = ({ category }) => {
+const posts = useSelector((state) => postsByCategory(state, category));
 
 return (
     <Row>
     {posts.map((post) => (
-    <Col xs={12} md={6} lg={4} key={post.id} className="my-3">
-        <Card style={{ width: '100%', height: '100%'}}>
-        <Card.Body>
+        <Col key={post.id} className="my-3">
+        <Card>
+            <Card.Body>
             <Card.Title>{post.title}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{post.publishedDate}</Card.Subtitle>
             <Card.Text className='pt-2 mb-0'><strong>Author:</strong> {post.author}</Card.Text>
             <Card.Text><strong>Category:</strong>{post.category}</Card.Text>
             <Card.Text><strong>Description:</strong> <span dangerouslySetInnerHTML={{ __html: post.content }}></span></Card.Text>
-            <Button variant="primary" href={`/post/${post.id}`}>Read More</Button>
-        </Card.Body>
+            <Button variant="warning" href={`/post/${post.id}`}>Read More</Button>
+            </Card.Body>
         </Card>
-    </Col>
+        </Col>
     ))}
-</Row>
+    </Row>
 )
 };
 
-export default GenerateCards;
+export default CategoryGenerateCards;
